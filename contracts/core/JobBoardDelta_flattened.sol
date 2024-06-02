be95f7923f74a1e8c4163fa8397a591340c5773b
Cryptotwilight <tonyusheuk@gmail.com>
2024-06-02 14:49:00 +0200

    Updated

--- contracts/core/JobBoardDelta_flattened.sol
+++ contracts/core/JobBoardDelta_flattened.sol
@@ -342,12 +342,25 @@ interface IJobBoardDelta {
 
     function postJob(string memory _cid, Duration _duration) payable external returns (Job memory _job);
 
+    function hasReward(uint256 _jobId) view external returns (bool _hasReward);
 
+    function getPrice(Duration _duration)  view external returns (uint256 _price);
 
+    function getPaymentErc20() view external returns (address _paymentErc20);
 
+    function getJobReward(uint256 _jobId) view external returns (Reward memory _reward);
 
+    function getApplicant(uint256 _applicantId) view external returns (Applicant memory _applicant);
 
+    function applyForJob(uint256 _jobId, string memory _resumeCid, uint256 _rewardChainId, address _rewardAddress, bool _isReferral) external returns (uint256 _applicantCount);
 
+    function getApplicantIds(uint256 _jobId) view external returns (uint256 [] memory _jobIds);
+
+    function approveRewards(uint256 _jobId, uint256 [] memory _applicantIds) external returns (uint256 _rewardedCount);
+
+    function addReward(uint256 _jobId, address _erc20, uint256 _rewardAmount, uint256 _amountPerApplicant) payable external returns (Reward memory _reward);
+
+    function getOwnerJobIds() view external returns (uint256 [] memory _jobIds);
 
 }
 
